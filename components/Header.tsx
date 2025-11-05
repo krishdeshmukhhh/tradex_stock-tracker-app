7a6d2573af30913797039c1235042ce67d0f1135
Krish Deshmukh <deshmukh.102@osu.edu>
2025-11-05 16:32:15 -0500

    Implement auth logic

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import NavItems from "@/components/NavItems";
 import UserDropdown from './UserDropdown';
 
-const Header = () => {
+const Header = ({ user }: {user: User}) => {
     return (
         <header className="sticky top-0 header">
             <div className="container header-wrapper">
@@ -16,7 +16,7 @@ const Header = () => {
                 <NavItems/>
                 </nav>
 
-                <UserDropdown/>
+                <UserDropdown user={user} />
             </div>
         </header>
     )
